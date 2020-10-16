9ce13bf25a616b2162dec7cb152326d243fc5b76
Cyril TOURNIER <tourniercy@gmail.com>
2020-10-16 14:06:34 +0200

    Update Apidata.vue

--- src/components/Apidata.vue
+++ src/components/Apidata.vue
@@ -2,19 +2,19 @@
 
   <v-container>
     <v-row no-gutters v-if="loading">
-        <v-col
-            v-for="i in 10" :key="i"
-            cols="12"
-            sm="4"
-        >
-          <v-skeleton-loader
-              class="mx-auto"
-              max-width="344"
-              type="card"
-          ></v-skeleton-loader>
-        </v-col>
+      <v-col
+          v-for="i in 10" :key="i"
+          cols="12"
+          sm="4"
+      >
+        <v-skeleton-loader
+            class="mx-auto"
+            max-width="344"
+            type="card"
+        ></v-skeleton-loader>
+      </v-col>
     </v-row>
-    <v-row no-gutters>
+    <v-row no-gutters v-else>
       <v-col
           v-for="character in characters" :key="character.id"
           cols="12"
@@ -49,9 +49,12 @@
     </v-row>
     <div class="text-center">
       <v-pagination
-          v-model="offset"
+          v-model="page"
           :length="total"
+          total-visible="10"
           circle
+          @input="updatePagination"
+
       ></v-pagination>
     </div>
   </v-container>
@@ -64,25 +67,38 @@ export default {
   data() {
     return {
       offset : 1,
+      page: 1,
       total: 0,
       show: null,
       loading: true,
       characters: []
     }
   },
+
+  methods:{
+    updatePagination (pagination) {
+      this.loading = true;
+      this.offset = pagination*20-20
+      this.page = pagination
+      this.getCharacters()
+    },
+    getCharacters: function() {
+      axios
+          .get('http://gateway.marvel.com/v1/public/characters?orderBy=name&offset='+this.offset+'&ts=1&apikey=5f30a789bead9d41e5a18b34f8c68733&hash=e1598d387c7946ba66079f451ae93788')
+          .then(response => {
+            console.log(response)
+            this.characters = response.data.data.results
+            this.total = parseInt(response.data.data.total/20)
+            this.loading = false
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          .finally(() => this.loading = false)
+    }
+  },
   mounted() {
-    axios
-        .get('http://gateway.marvel.com/v1/public/characters?orderBy=name&offset='+this.offset+'&ts=1&apikey=5f30a789bead9d41e5a18b34f8c68733&hash=e1598d387c7946ba66079f451ae93788')
-        .then(response => {
-          console.log(response)
-          this.characters = response.data.data.results
-          this.total = parseInt(response.data.data.total/20)
-          this.loading = false
-        })
-        .catch(error => {
-          console.log(error)
-        })
-        .finally(() => this.loading = false)
+    this.getCharacters()
   }
 }
 </script>
